ac53e5b08ecbbd17b069a751ced526e15c18fd41
RafalDrgas <rafinho91@gmail.com>
2018-02-22 21:01:44 +0100

    new while loop

--- src/trzecie/WhileLoop.java
+++ src/trzecie/WhileLoop.java
@@ -2,11 +2,113 @@ package trzecie;
 
 import java.util.Scanner;
 
-//wczytuje liczby az nie bedzie ona podzielna przez 15, wyswietlamy te liczbe
-//wczytuje az ktos nie poda 0, wyswietlamy sume
 public class WhileLoop {
     public static void main(String[] args) {
-        ThirdLoop();
+        Scanner scanner = new Scanner(System.in);
+        int option = 1;
+        while (option != 0) {
+            System.out.println("1. New");
+            System.out.println("2. Load");
+            System.out.println("3. About");
+            System.out.println("0. Quit");
+
+            option = scanner.nextInt();
+            System.out.println();
+        }
+    }
+
+    private static void Six() {
+        System.out.println("Check value if primary: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int i = 2;
+        boolean isPrimary = true;
+        while(isPrimary && i <= value/2) {
+            if ((value%i) == 0) {
+                isPrimary = false;
+            }
+            i++;
+        }
+        if(isPrimary) {
+            System.out.println("Number is primary");
+        } else {
+            System.out.println("Number is not primary");
+        }
+    }
+
+    private static void Five() {
+        System.out.println("Insert value you want to divide: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        for(int i = 1; i <= value/2; i++) {
+            if ((value%i) == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static void Four() {
+        System.out.println("How many digits in the number");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert value: ");
+        int value = scanner.nextInt();
+        System.out.println("Insert digit: ");
+        int digit = scanner.nextInt();
+        int numOfDigit = 0;
+        if (value == 0 && digit == 0){
+            numOfDigit++;
+        }
+        while(value != 0) {
+            int rest = value % 10;
+            if (rest == digit) {
+                numOfDigit++;
+            }
+            value /= 10;
+        }
+        System.out.println("Digit " + digit + " appears " + numOfDigit + ". times");
+    }
+
+    private static void Three() {
+        System.out.println("Looking for the sum of digits");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int sum = 0;
+        while(value != 0) {
+            int rest = value % 10;
+            sum = sum + rest;
+            value /= 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+
+    private static void Two() {
+        System.out.println("Looking for the number of digits");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int i = 0;
+        if (value == 0) {
+            i++;
+        }
+        while(value != 0) {
+            i++;
+            value = value / 10;
+        }
+        System.out.println("Number of digits is: " + i);
+    }
+
+    private static void One() {
+        System.out.println("Looking for the biggest number of given");
+        Scanner scanner = new Scanner(System.in);
+        int max = 0;
+        boolean result = true;
+        while (result) {
+            int value = scanner.nextInt();
+            result = value != 0;
+            if (value > max) {
+                max = value;
+            }
+        }
+        System.out.println("The biggest value of given is " + max);
     }
 
     private static void ThirdLoop() {
